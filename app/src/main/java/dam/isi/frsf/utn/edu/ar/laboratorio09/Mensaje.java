b4d3b4882eb9e07f2345e33dffef4dcb8510612e
Luciano Gambino <lucho_gambino@hotmail.com>
2017-02-22 08:17:03 -0300

    Inicio

---
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/laboratorio09/Mensaje.java
@@ -0,0 +1,42 @@
+package dam.isi.frsf.utn.edu.ar.laboratorio09;
+
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class Mensaje extends FirebaseInstanceIdService {
+
+
+    public Mensaje() {
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTokenRefresh() {
+// obtener token InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        saveTokenToPrefs(refreshedToken);
+    }
+
+    private void saveTokenToPrefs(String _token){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("registration_id", _token);
+        editor.apply();
+
+    // luego en cualquier parte de la aplicación podremos recuperar el token con
+    // SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+    // preferences.getString("registration_id", null);
+
+    }
+}
+
+
